811e0864faeb3b0cc71b3a47328871c6537dbdc5
8thGear-Tech <opeyemibioku@gmail.com>
2022-11-24 14:53:17 +0100

    Updated the new bg color on the underconstruction page

--- src/components/Buttons/actionBtn.js
+++ src/components/Buttons/actionBtn.js
@@ -1,12 +1,12 @@
 //external import
 import Button from "react-bootstrap/Button";
 
-export const JoinOurListBtn = () => {
+export const BookaSessionBtn = () => {
   return (
     <>
       <a href="https://bit.ly/MindafrikCounsellingSession" target="_blank">
         {" "}
-        <Button className="JoinOurListBtn">Join our List</Button>
+        <Button className="BookaSessionBtn">Book a Session</Button>
       </a>
     </>
   );
--- src/pages/underConstruction.js
+++ src/pages/underConstruction.js
@@ -2,7 +2,7 @@
 import { NavbarSection } from "../components/navbar";
 
 //buttons
-import { JoinOurListBtn } from "../components/Buttons/actionBtn";
+import { BookaSessionBtn } from "../components/Buttons/actionBtn";
 
 //images
 import underconstructionhero from "../assets/images/hero/underconstructionhero.png";
@@ -22,58 +22,63 @@ const UnderConstruction = () => {
 const SectionOne = () => {
   return (
     <>
-      <div className="px-4 container-fluid underconstructionPageBg pb-4">
+      <div className="px-3">
+        {" "}
         <NavbarSection />
-        <div className="row align-items-center justify-content-center mt-4">
-          <div className="col-lg-6 col-md-7 col-sm-12 ">
-            <h1 className="headerFontColor">Launching soon!</h1>
-            <h4 className="headerFontColor">Thank you for visiting.</h4>
-            <p className="d-flex">
-              At Mindafrik, our deepest purpose and drive is helping to support
-              the stability of people struggling with disturbed minds,confused
-              and dark thoughts through supportive listening and professional
-              counseling for their best life ahead.
-            </p>
-            <div className="d-flex">
-              {" "}
-              <PhoneIcon />
-              <a href="tel:2348133199953" className="text-decoration-none">
-                <p className="paragraphColor mx-3">+2348133199953</p>
-              </a>
+        <div className="px-4 container-fluid underconstructionPageBg pb-4">
+          <div className="row align-items-center justify-content-center mt-3">
+            <div className="col-lg-6 col-md-12 col-sm-12 ">
+              <h1 className="headerFontColor d-none d-xl-block d-lg-block d-md-block d-sm-none d-xs-none">
+                Launching Our Site Soon!
+              </h1>
+              <h2 className="headerFontColor d-xs-block d-sm-block d-md-none d-lg-none d-xl-none">
+                Launching Our Site Soon!
+              </h2>
+              <h4 className="headerFontColor">Thank you for visiting.</h4>
+              <p className="d-flex">
+                At Mindafrik, our deepest purpose and drive is in empowering
+                people to create and live the life they want through supportive
+                listening and professional counselling.
+              </p>
+              <h3 className="headerFontColor">Our services are focused on:</h3>
+              <div className=" ">
+                <ul>
+                  <li class="paragraphText ">
+                    Relationship, Family and Marriage counselling
+                  </li>
+                  <li class="paragraphText ">Trauma Informed Care and</li>
+                  <li class="paragraphText ">Mental Health</li>
+                </ul>
+              </div>
+              <div className="text-center mb-4">
+                <BookaSessionBtn />
+              </div>
             </div>
-            <div className="d-flex">
-              <Mailicon />{" "}
-              <a
-                href="mailto:info@mindafrik.com"
-                className="text-decoration-none"
-              >
-                <p className="paragraphColor mx-3">info@mindafrik.com</p>
-              </a>
+            <div className="col-lg-5 col-md-12 col-sm-10 mb-5">
+              <img
+                src={underconstructionhero}
+                className="mx-lg-auto img-fluid "
+                alt=""
+                loading=""
+              />
             </div>
-          </div>
-          <div className="col-lg-5 col-md-5 col-sm-10">
-            <img
-              src={underconstructionhero}
-              className="mx-lg-auto img-fluid"
-              alt=""
-              loading=""
-            />
-          </div>
-          <div className="pt-5">
-            <h3 className="headerFontColor text-center">
-              Our services are focused on:
-            </h3>
-            <div className="d-flex justify-content-center ">
-              <ul>
-                <li class="paragraphText ">
-                  Relationship, Family and Marriage counselling
-                </li>
-                <li class="paragraphText ">Trauma Informed Care and</li>
-                <li class="paragraphText ">Mental Health</li>
-              </ul>
-            </div>
-            <div className="text-center">
-              <JoinOurListBtn />
+            <div className="pt-3">
+              <div className="d-flex justify-content-center">
+                {" "}
+                <PhoneIcon />
+                <a href="tel:2348133199953" className="text-decoration-none">
+                  <p className="paragraphColor mx-3">+2348133199953</p>
+                </a>
+              </div>
+              <div className="d-flex justify-content-center">
+                <Mailicon />{" "}
+                <a
+                  href="mailto:info@mindafrik.com"
+                  className="text-decoration-none"
+                >
+                  <p className="paragraphColor mx-3">info@mindafrik.com</p>
+                </a>
+              </div>
             </div>
           </div>
         </div>
