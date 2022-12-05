13d747c887562f920036ec42baa0a53ca77a49a5
8thGear-Tech <opeyemibioku@gmail.com>
2022-12-05 16:34:57 +0100

    worked on the admin statistics card

--- src/App.js
+++ src/App.js
@@ -3,14 +3,14 @@ import * as React from "react";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 
 import UnderConstruction from "./pages/websitePages/underConstruction";
-import { EvaluationCard } from "./components/allCards/PsychologicalAssessment";
+// import { EvaluationCard } from "./components/allCards/PsychologicalAssessment";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<UnderConstruction />} />
-        <Route path="cards" element={<EvaluationCard />} />
+        {/* <Route path="cards" element={<EvaluationCard />} /> */}
       </Routes>
     </BrowserRouter>
   );
