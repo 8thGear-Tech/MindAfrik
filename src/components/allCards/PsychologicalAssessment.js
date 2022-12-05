3fe697be5648806b8d53510ddc0c7c6840fc6f62
8thGear-Tech <opeyemibioku@gmail.com>
2022-12-05 16:32:13 +0100

    worked on the admin statistics cards

--- src/components/allCards/PsychologicalAssessment.js
+++ src/components/allCards/PsychologicalAssessment.js
@@ -1,3 +1,32 @@
 // evaluationCards
 // gotYouCoveredCards
 //
+
+import Card from "react-bootstrap/Card";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+
+import statone from "../../assets/images/stress-evaluation.png";
+
+export const EvaluationCard = (props) => {
+  const { headingone, headingtwo, paragraphtext, icon } = props;
+  return (
+    <>
+      <Row xs={1} md={2} className="g-4">
+        <Col>
+          <Card className="w-50">
+            <Card.Img variant="top" src={statone} />
+            <Card.Body className="hello">
+              <Card.Title classNam="text-center"> Stress Evaluation</Card.Title>
+              {/* <Card.Text>
+                This is a longer card with supporting text below as a natural
+                lead-in to additional content. This content is a little bit
+                longer.
+              </Card.Text> */}
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+    </>
+  );
+};
--- src/components/allCards/dashBoardcards.js
+++ src/components/allCards/dashBoardcards.js
@@ -4,4 +4,77 @@
 // counseleeProfilePageCard
 // viewProfileCard
 // counseleeSessionPageCard
-//AdminStatisticsCards
+
+import Card from "react-bootstrap/Card";
+import adminStats from "../../data/allCards/dashboardCards.json";
+
+export const AdminStatisticsCard = (props) => {
+  const { headingone, headingtwo, paragraphtext, icon } = props;
+  return (
+    <>
+      <Card border="" style={{ width: "25rem" }} className="cardShadow">
+        <div className="d-flex align-items-center ms-2 me-4 my-auto">
+          {" "}
+          <Card.Body>
+            {" "}
+            <Card.Title>{headingone}</Card.Title>
+            <Card.Title>{headingtwo}</Card.Title>
+            <Card.Text>{paragraphtext}</Card.Text>
+          </Card.Body>
+          <Card.Img
+            variant="top"
+            src={icon}
+            className="mx-lg-auto img-fluid"
+            style={{ maxHeight: "100px", maxWidth: "120px" }}
+          />
+        </div>
+      </Card>
+      <br />
+    </>
+  );
+};
+
+export const AdminStatistics = () => {
+  return (
+    <>
+      <div className="container-fluid">
+        <div className="row justify-content-center mx-5 pt-5">
+          {adminStats.adminStatsInfo.map((stats) => {
+            return (
+              <>
+                <div
+                  className="col-lg-4 col-md-4 col-sm-10 my-4"
+                  key={stats.id}
+                >
+                  <AdminStatisticsCard {...stats} />
+                </div>
+              </>
+            );
+          })}
+        </div>
+      </div>
+    </>
+  );
+};
+
+// <div className="px-3">
+//   <div className="px-4 container-fluid pb-4">
+//     <div className="row mt-3">
+//       <div className="col-lg-6 col-md-12 col-sm-12 ">
+//         <h1 className="">5,678</h1>
+//         <h4 className="">Total sign up Users</h4>
+//         <p className="">+0.5% than last Month</p>
+//       </div>
+//       <div className="col-lg-5 col-md-12 col-sm-10 mb-5">
+//         <img
+//           src={statone}
+//           className="mx-lg-auto img-fluid "
+//           alt=""
+//           loading=""
+//         />
+//       </div>
+//     </div>
+//   </div>
+// </div>
+//   );
+// };
