0c5131fb706dfb014bf04cb12b227e8ea2dea5e0
Awelewa165 <oluwatoyinawelewa7@gmail.com>
2023-01-20 12:13:58 +0100

    screeen responsiveness

--- src/components/hero.js
+++ src/components/hero.js
@@ -1,28 +1,26 @@
 // HomePageHero
 // AboutUsHero
 // PsychologicalAssessmentHero
-import PsychologicalAssessmentphoto from "../Image/Psychological Assessment Hero .png";
+import AssessmentHero from "../../src/Image/AssesmmentHero.png";
 
 const PsychologicalAssessmentHero = () => {
   return (
     <>
-      <div className="container">
+      <div className="container pt-3 px-4">
         <div className=" row">
-          <div className="col-6">
-            <h4>You don't feel good?</h4>
-            <h6>We can help you find yourself</h6>
+          <div className="col-lg-6 col-md-6 col-sm-12 mt-5 align-items-center">
+            <h1>You don't feel good?</h1>
+            <br></br>
+            <h4>We can help you find yourself</h4>
+            <br></br>
             <p>
               Mental test is self care and development for healthy connection
               between your soul and body. Mental test is self care and
               development for healthy connection between your soul and body.
             </p>
           </div>
-          <div className="col-6">
-            <img
-              style={{ width: "20rem" }}
-              src={PsychologicalAssessmentphoto}
-              className=" AssessmentPhoto"
-            />
+          <div className="col-lg-6 col-md-6 col-sm-12 ">
+            <img src={AssessmentHero} />
           </div>
         </div>
       </div>
@@ -30,17 +28,3 @@ const PsychologicalAssessmentHero = () => {
   );
 };
 export default PsychologicalAssessmentHero;
-{
-  /* <div className="row ">
-        <div className="col-lg-6 col-md-12 col-sm-12 ">
-          <p className="d-flex">
-            Mental test is self care and development for healthy connection
-            between your soul and body. Mental test is self care and development
-            for healthy connection between your soul and body
-          </p>
-        </div>
-      </div>
-      <div className="col-lg-5 col-md-12 col-sm-10 mb-5">
-        <img>src={PsychologicalAssessmentphoto}</img>
-      </div> */
-}
