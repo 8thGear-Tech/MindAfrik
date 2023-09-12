2d668c4f6a94c4d7fa6abfc75ade22ba2a503f26
opeyemibioku <opeyemibioku@gmail.com>
2023-09-12 10:16:12 +0100

    implemented role based authorization

--- src/components/requireAuth.js
+++ src/components/requireAuth.js
@@ -1,3 +1,29 @@
+// import { useLocation, Navigate, Outlet } from "react-router-dom";
+// import useAuth from "./hooks/useAuth";
+
+// const RequireAuth = ({ allowedRoles }) => {
+//   const { auth } = useAuth();
+//   const location = useLocation();
+
+//   if (
+//     (auth?.roles.includes(ROLES.Counsellor) &&
+//       allowedRoles.includes(ROLES.Counsellor)) ||
+//     (auth?.roles.includes(ROLES.Admin) && allowedRoles.includes(ROLES.Admin)) ||
+//     (auth?.roles.includes(ROLES.Counsellee) &&
+//       allowedRoles.includes(ROLES.Counsellee))
+//   ) {
+//     return <Outlet />;
+//   }
+
+//   return auth?.user ? (
+//     <Navigate to="/unauthorized" state={{ from: location }} replace />
+//   ) : (
+//     <Navigate to="/signInPage" state={{ from: location }} replace />
+//   );
+// };
+
+// export default RequireAuth;
+
 import { useLocation, Navigate, Outlet } from "react-router-dom";
 import useAuth from "./hooks/useAuth";
 
