4bfb74eccb0aaa69a31e1eac74155beff020b802
opeyemibioku <opeyemibioku@gmail.com>
2023-09-12 15:33:06 +0100

    updates

--- src/components/requireAuth.js
+++ src/components/requireAuth.js
@@ -34,7 +34,10 @@ const RequireAuth = ({ allowedRoles }) => {
   console.log("Auth:", auth);
   console.log("Allowed Roles:", allowedRoles);
   // return auth?.role?.find((role) => allowedRoles?.includes(role)) ? (
-  return auth?.role && allowedRoles.includes(auth?.role) ? (
+  // return auth?.role && allowedRoles.includes(auth?.role) ? (
+  return auth?.role &&
+    allowedRoles.includes(auth?.role) &&
+    auth?.access_token ? (
     <Outlet />
   ) : auth?.user ? (
     <Navigate to="/unauthorized" state={{ from: location }} replace />
