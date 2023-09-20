b4cab4405a884898f2fb61a48f3943330e6538b0
opeyemibioku <opeyemibioku@gmail.com>
2023-09-20 17:22:45 +0100

    updates

--- src/components/requireAuth.js
+++ src/components/requireAuth.js
@@ -5,16 +5,37 @@ import useAuth from "./hooks/useAuth";
 const RequireAuth = ({ allowedRoles }) => {
   const { auth } = useAuth();
   const location = useLocation();
-  const accessToken = Cookies.get("access_token");
-  console.log("Auth:", auth);
-  console.log("Allowed Roles:", allowedRoles);
-  console.log("Access Token:", accessToken);
-  return accessToken && allowedRoles.includes(auth?.role) ? (
-    <Outlet />
-  ) : (
-    <Navigate to="/signInPage" state={{ from: location }} replace />
-  );
+  const storedAccessToken = Cookies.get("access_token");
+
+  if (!auth?.role && !storedAccessToken) {
+    // Neither role nor access token exists, redirect to sign-in
+    return <Navigate to="/signInPage" state={{ from: location }} replace />;
+  }
+
+  if (!auth?.role && storedAccessToken) {
+    // Role doesn't exist but access token exists, consider user authenticated
+    // You might want to decode and verify the token server-side here
+    return <Outlet />;
+  }
+
+  if (auth?.role && allowedRoles.includes(auth?.role)) {
+    // User has the required role
+    return <Outlet />;
+  }
+
+  // User has a role, but it's not in the allowedRoles
+  return <Navigate to="/unauthorized" state={{ from: location }} replace />;
 };
+//   const accessToken = Cookies.get("access_token");
+//   console.log("Auth:", auth);
+//   console.log("Allowed Roles:", allowedRoles);
+//   console.log("Access Token:", accessToken);
+//   return accessToken && allowedRoles.includes(auth?.role) ? (
+//     <Outlet />
+//   ) : (
+//     <Navigate to="/signInPage" state={{ from: location }} replace />
+//   );
+// };
 
 //   return auth?.role && allowedRoles.includes(auth?.role) ? (
 //     <Outlet />
