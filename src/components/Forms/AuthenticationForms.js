7be02d11a5045149b8ea3045fe7885b0668fa7b8
opeyemibioku <opeyemibioku@gmail.com>
2023-09-26 12:21:40 +0100

    updates

--- src/components/Forms/AuthenticationForms.js
+++ src/components/Forms/AuthenticationForms.js
@@ -143,42 +143,41 @@ export const SignInForm = () => {
     try {
       const response = await axios.post(
         "https://mindafrikserver.onrender.com/user/login",
-        JSON.stringify({
+        {
           email: email,
           password: password,
-        }),
-
+        },
         {
           headers: { "Content-Type": "application/json" },
           withCredentials: true,
         }
       );
 
-      console.log("Response:", response?.data);
+      // console.log("Response:", response);
 
-      // const { data } = response;
+      const { data } = response;
 
-      // if (data.status === "Success") {
-      // const { role, access_token } = data.data;
-      const access_token = response?.data?.access_token;
-      const role = response?.data?.role;
-      console.log("Access Token:", access_token);
-      console.log("Role:", role);
-      setAuth({ email, password, role, access_token });
-      // setAuth({ email, password, role, access_token }, true);
-      // setUser("");
-      // setPwd("");
-      navigate(from, { replace: true });
-      //   if (role === "Counsellor") {
-      //     navigate("/counsellorDashboard", { replace: true });
-      //   } else if (role === "Admin") {
-      //     navigate("/adminDashboard", { replace: true });
-      //   } else if (role === "Counselee") {
-      //     navigate("/counselleeDashboard", { replace: true });
-      //   } else {
-      //     navigate("/unauthorized", { replace: true });
-      //   }
-      // }
+      if (data.status === "Success") {
+        const { role, access_token } = data.data;
+        // const access_token = response?.data?.access_token;
+        // const role = response?.data?.role;
+        console.log("Access Token:", access_token);
+        console.log("Role:", role);
+        // setAuth({ email, password, role, access_token });
+        setAuth({ email, password, role, access_token }, true);
+        // setUser("");
+        // setPwd("");
+        // navigate(from, { replace: true });
+        if (role === "Counsellor") {
+          navigate("/counsellorDashboard", { replace: true });
+        } else if (role === "Admin") {
+          navigate("/adminDashboard", { replace: true });
+        } else if (role === "Counselee") {
+          navigate("/counselleeDashboard", { replace: true });
+        } else {
+          navigate("/unauthorized", { replace: true });
+        }
+      }
 
       // const { data } = response; // Destructure the data from the response
 
@@ -309,7 +308,7 @@ export const SignInForm = () => {
                   {err}
                 </p>
               )}
-              <div className="persistCheck">
+              {/* <div className="persistCheck">
                 <input
                   type="checkbox"
                   id="persist"
@@ -317,7 +316,7 @@ export const SignInForm = () => {
                   checked={persist}
                 />
                 <label htmlFor="persist">Trust This Device</label>
-              </div>
+              </div> */}
             </div>
           </Form>
         )}
